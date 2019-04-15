2e6348a609ffd02521c8a99f18246d601868606e
Nicodie <nicodiemartin@live.com>
2019-04-15 18:49:56 -0400

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';//needed for templating
 import { HttpClientModule } from '@angular/common/http';//needed for ajax
 import { RouterModule } from '@angular/router';//needed for routing
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';//need for angular material
-import {MatExpansionModule } from '@angular/material';//an angular material component we will use in templating
-
 
 import { AppComponent } from './app.component';
 import { PokemonComponent} from './pokemon/pokemon.component';
@@ -20,8 +17,6 @@ const routes = [
   imports:[ 
       BrowserModule, 
       FormsModule,
-      BrowserAnimationsModule,//dependency for angular material
-      MatExpansionModule,//our new ui component
 	  HttpClientModule,//import our ajax module
       RouterModule.forRoot(routes)//add our routes to the application
   ],
