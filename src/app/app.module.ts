f0a0ce46125dfb4d9f429e5c8af43f85d87cc6c9
Nicodie <nicodiemartin@live.com>
2019-04-15 18:24:56 -0400

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';//needed for templating
 import { HttpClientModule } from '@angular/common/http';//needed for ajax
+import { RouterModule } from '@angular/router';//needed for routing
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { PokemonComponent} from './pokemon/pokemon.component';
 
-import { PokemonComponent} from './pokemon/pokemon.component'; //add this line
+//angular routes
+const routes = [
+  {path:'pokemon', component: PokemonComponent, data: {title:'List of Pokemon'}},
+  {path: "**", redirectTo: '/pokemon'}
+];
 
 @NgModule({
-  imports:      [ 
-	  BrowserModule, 
-	  FormsModule,
-	  HttpClientModule //import our ajax module
+  imports:[ 
+      BrowserModule, 
+      FormsModule,
+	  HttpClientModule,//import our ajax module
+      RouterModule.forRoot(routes)//add our routes to the application
   ],
-  declarations: [ AppComponent, HelloComponent, PokemonComponent ],//add PokemonComponent to this array
+  declarations: [ AppComponent, PokemonComponent ],//add PokemonComponent to this array
   bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
