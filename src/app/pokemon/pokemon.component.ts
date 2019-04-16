c80aaec7e27daf49c31fd7567c25c17b1202b159
Nicodie <nicodiemartin@live.com>
2019-04-16 08:46:33 -0400

    1 file modified

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,25 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-//standard imports
-
-const BASE_URL = 'https://pacific-headland-85124.herokuapp.com';//endpont url
 
 //standard component boilerplate code
 @Component({
   selector: 'app-pokemon',
-  templateUrl: './pokemon.component.html',
-  styleUrls: ['./pokemon.component.css']
+  templateUrl: './pokemon.component.html',//associated html file
+  styleUrls: ['./pokemon.component.css']//associated css file
 })
-export class PokemonComponent {
-  
-  records;
-
-  constructor(private http:HttpClient) { }//inject module into component
-
-  ngOnInit() {
-    //use module to perfrom ajax, must use arrow anonymous function
-    this.http.get(BASE_URL + "/api/pokemon").subscribe(data=>{
-      this.records = data;//set records to data received 
-    })
-  }
+export class PokemonComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {}
 }
